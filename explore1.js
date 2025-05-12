a324ab4ec4cb5a41199758b453ff5de8d512d611
Pradnyaaaa <pradnyasardar2005@gmail.com>
2025-05-12 16:50:55 +0530

    Add files via upload

---
+++ explore1.js
@@ -0,0 +1,16 @@
+console.log("hello");
+
+
+function sayhello(){
+    console.log("i would like to say hello");
+  
+    setTimeout(() => {
+        sayhello();
+    }, 2000);
+}
+
+for (let index = 0; index < array.length; index++) {
+    console.log(index);
+    
+    
+}
